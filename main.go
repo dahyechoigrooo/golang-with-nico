27b495e6850adbc49a43f7718b6780b08e97e861
Dahye Choi <>
2022-01-17 17:35:25 +0900

    JOB SCRAPER v1

--- main.go
+++ main.go
@@ -1,65 +1,109 @@
 package main
 
 import (
-	"errors"
 	"fmt"
+	"log"
 	"net/http"
+	"strconv"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
-// 요청에 대한 결과값을 구조체를 만들어 담는다.
-type RequestResult struct {
-	url    string
-	status string
+type extractedJob struct {
+	id       string
+	title    string
+	location string
+	salary   string
+	summary  string
 }
 
-// 에러 메세지를 변수에 담아 재활용할 수 있게 만든다.
-var errRequestFailed = errors.New("Request Failed")
+var baseURL string = "https://kr.indeed.com/jobs?q=python&limit=50"
 
 func main() {
-	// 결과 값을 map 함수로 만들어 key-value 형태의 변수를 만든다.
-	results := make(map[string]string)
-	// channel을 사용하여 메세지를 담을 변수를 만든다.
-	c := make(chan RequestResult)
-
-	// URL Checker에서 사용할 URL을 배열에 담는다.
-	urls := []string{
-		"https://www.airbnb.com/",
-		"https://www.google.com/",
-		"https://www.amazon.com/",
-		"https://www.reddit.com/",
-		"https://www.google.com/",
-		"https://soundcloud.com/",
-		"https://www.facebook.com/",
-		"https://www.instagram.com/",
-		"https://academy.nomadcoders.co/",
-	}
-
-	// 배열 안에 담긴 URL을 반복문으로 돌린다. 이때 URL은 goroutines를 사용하여 동시에 코드를 실행한다.
-	for _, url := range urls {
-		go hitURL(url, c)
-	}
-
-	// 메세지는 goroutines의 개수만큼 받아야한다. 일일이 개수를 세지 않고 URL의 개수만큼 반복문을 돌려 메세지를 받는다.
-	for i := 0; i < len(urls); i++ {
-		result := <-c
-		results[result.url] = result.status
-	}
-
-	// URL Checking 후의 결과 값을 url, status 형태로 출력한다.
-	for url, status := range results {
-		fmt.Println(url, status)
+	var jobs []extractedJob
+	totalPages := getPages()
+	for i := 0; i < totalPages; i++ {
+		extractJobs := getPage(i)
+		jobs = append(jobs, extractJobs...)
+		fmt.Println(jobs)
 	}
 }
 
-// RequestResult 라는 이름으로 메세지를 받는다(channel)
-func hitURL(url string, c chan<- RequestResult) {
-	resp, err := http.Get(url)
-	status := "OK"
-	// status 기본값이 'OK'이고 에러가 났을때만 'FAILED'를 출력한다.
-	if err != nil || resp.StatusCode >= 400 {
-		fmt.Println(err, resp.StatusCode)
-		status = "FAILED"
+// 각각의 페이지의 URL주소9
+func getPage(page int) []extractedJob {
+	var jobs []extractedJob
+	pageURL := baseURL + "&start=" + strconv.Itoa(page*50)
+	fmt.Println("Requesting" + pageURL)
+	resp, err := http.Get(pageURL)
+	checkErr(err)
+	checkCode(resp)
+
+	defer resp.Body.Close()
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	checkErr(err)
+
+	searchCards := doc.Find(".tapItem")
+
+	searchCards.Each(func(i int, card *goquery.Selection) {
+		job := extractJob(card)
+		jobs = append(jobs, job)
+	})
+
+	return jobs
+
+}
+
+// 각 취업 공고의 정보를 스크래핑하는 함수(struct 반환)
+func extractJob(card *goquery.Selection) extractedJob {
+	id, _ := card.Attr("data-jk")
+	title := cleanString(card.Find(".jobTitle>span").Text())
+	location := cleanString(card.Find(".companyLocation").Text())
+	salary := cleanString(card.Find(".salary-snippet span").Text())
+	summary := cleanString(card.Find(".job-snippet").Text())
+	return extractedJob{
+		id:       id,
+		title:    title,
+		location: location,
+		salary:   salary,
+		summary:  summary}
+}
+
+// 공백을 제거해주는 함수
+func cleanString(str string) string {
+	return strings.Join(strings.Fields(strings.TrimSpace(str)), " ")
+}
+
+// HTML 파싱해오는 함수
+func getPages() int {
+	pages := 0
+	resp, err := http.Get(baseURL)
+	checkErr(err)
+	checkCode(resp)
+
+	// 함수가 종료된 후에 닫아줘서 메모리가 새어나가는 것을 막는다.
+	defer resp.Body.Close()
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	checkErr(err)
+
+	fmt.Println(doc)
+
+	doc.Find(".pagination").Each(func(i int, s *goquery.Selection) {
+		pages = s.Find("a").Length()
+	})
+	return pages
+}
+
+// Goquery 도큐먼트에 대한 에러 검사
+func checkErr(err error) {
+	if err != nil {
+		log.Fatalln(err) // 프로그램 종료
 	}
-	// status가 'OK'라면 key-value 형태의 데이터를 메시지로 메인에 전달한다.
-	c <- RequestResult{url: url, status: status}
+}
+
+// Get에 대한 에러 검사
+func checkCode(resp *http.Response) {
+	if resp.StatusCode != 200 {
+		log.Fatalln("Request failed withStatus : ", resp.StatusCode)
+	}
 }
